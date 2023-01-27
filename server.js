6b0ccf71c3cd6d7f0922858580dea9e8ffec26dd
Michael Zogin <mdzogin@gmail.com>
2023-01-26 21:50:14 -0500

    added build

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 const express = require('express')
 // const router = express.Router()
+const path = require('path')
 const cors = require('cors')
 const nodemailer = require('nodemailer')
+// const { dirname } = require('path')
+// const { fileURLToPath } = require('url')
+
 const PORT = process.env.PORT || 5000
 
 const app = express()
 require('dotenv').config()
+
+app.use(cors())
+app.use(express.json())
+// const __dirname = dirname(fileURLToPath(import.meta.url))
 // when ready to deploy (after build)
 // !!!
+//serve the frontend
 app.use(express.static(path.resolve(__dirname, './client/build')))
 
 // app.get('*', (req, res) => {
 //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 // })
 
-app.use(cors())
-app.use(express.json())
 // ??
 // app.use('/', router)
 
@@ -136,4 +143,16 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
 })
 
-app.listen(PORT, () => console.log(`Server Running on port ${PORT}`))
+// app.listen(PORT, () => console.log(`Server Running on port ${PORT}`))
+
+const start = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}...`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
